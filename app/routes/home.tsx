6d2b3ea410eeed194565c7471babbda9d944881f
Shivendra <67376433+shivendrakc@users.noreply.github.com>
2026-03-13 22:03:14 +1100

    added files

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,14 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "Description", content: "Resumind is a platform for managing and analyzing resumes." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main>
+
+  </main>;
 }
